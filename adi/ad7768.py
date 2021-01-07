f649a72fc7ee8638e91c4470e210ba559933e930
Sergiu <sergiu.cuciurean@analog.com>
2021-01-07 18:58:59 +0200

    test

--- adi/ad7768.py
+++ adi/ad7768.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2019 Analog Devices, Inc.
+# Copyright (C) 2020 Analog Devices, Inc.
 #
 # All rights reserved.
 #
@@ -31,62 +31,26 @@
 # STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
 # THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from adi.ad936x import ad9361, ad9363, ad9364, Pluto
+from adi.context_manager import context_manager
+from adi.rx_tx import rx
+import numpy as np
 
-from adi.fmcomms5 import FMComms5
+class ad7768(rx, context_manager):
+    """ AD7768 Simultaneous Sampling ADC """
 
-from adi.ad9371 import ad9371
+    _rx_data_type = np.int32
+    _rx_data_si_type = np.int32
+    _rx_unbuffered_data = False
+    _rx_channel_names = ["voltage0", "voltage1", "voltage2", "voltage3",
+                         "voltage4", "voltage5", "voltage6", "voltage7"]
+    _device_name = ""
 
-from adi.adrv9002 import adrv9002
+    def __init__(self, uri="", ):
 
-from adi.adrv9009 import adrv9009
+        context_manager.__init__(self, uri, self._device_name)
 
-from adi.adrv9009_zu11eg import adrv9009_zu11eg
-
-from adi.adrv9009_zu11eg_multi import adrv9009_zu11eg_multi
-
-from adi.adrv9009_zu11eg_fmcomms8 import adrv9009_zu11eg_fmcomms8
-
-from adi.ad9081 import ad9081
-
-from adi.ad9094 import ad9094
-
-from adi.ad9680 import ad9680
-
-from adi.ad9144 import ad9144
-
-from adi.ad9152 import ad9152
-
-from adi.cn0532 import cn0532
-
-from adi.daq2 import DAQ2
-
-from adi.daq3 import DAQ3
-
-from adi.adis16460 import adis16460
-
-from adi.adis16507 import adis16507
-
-from adi.ad7124 import ad7124
-
-from adi.adxl345 import adxl345
-
-from adi.fmclidar1 import fmclidar1
-
-from adi.ad5686 import ad5686
-
-from adi.adar1000 import adar1000, adar1000_array
-
-from adi.ltc2983 import ltc2983
-
-from adi.one_bit_adc_dac import one_bit_adc_dac
-
-from adi.ad7768 import ad7768
-
-try:
-    from adi.jesd import jesd
-except ImportError:
-    pass
-
-__version__ = "0.0.7"
-name = "Analog Devices Hardware Interfaces"
+        self._rxadc = self._ctx.find_device("ad7768")
+        if not self._rxadc:
+                self._rxadc = self._ctx.find_device("cf_axi_adc")
+        
+        rx.__init__(self)
