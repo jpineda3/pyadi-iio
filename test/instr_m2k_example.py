2ff8bcfbb0a83dba9befbdae521d0ec5f603b10d
jpineda3 <Julia.Pineda@analog.com>
2023-05-05 09:38:33 +0800

    Default to voltmeter example
    Signed-off-by: jpineda3 <Julia.Pineda@analog.com>

--- test/instr_m2k_example.py
+++ test/instr_m2k_example.py
@@ -28,30 +28,30 @@ import instr_m2k
 
 # instr_m2k.contextClose(m2k_ctx)
 
-# ################### Voltmeter ###################
-# # Create voltmeter instrument from ip uri
-# m2k_uri='ip:192.168.2.1'
-# m2k_ctx = instr_m2k.connect(uri=m2k_uri,calibrate=False)
-# voltmeter = instr_m2k.create_instr(m2k_ctx,"voltmeter")
-
-# # Control signal generator
-# volt0 = instr_m2k.control(voltmeter,0)
-# print(f"Estimated frequency at channel 1: {volt0} V")
-# volt1 = instr_m2k.control(voltmeter,1)
-# print(f"Estimated frequency at channel 1: {volt1} V")
-
-# instr_m2k.contextClose(m2k_ctx)
-
-################### Power supply ###################
-# Create power supply instrument from ip uri
+################### Voltmeter ###################
+# Create voltmeter instrument from ip uri
 m2k_uri = "ip:192.168.2.1"
 m2k_ctx = instr_m2k.connect(uri=m2k_uri, calibrate=False)
-ps = instr_m2k.create_instr(m2k_ctx, "powersupply")
+voltmeter = instr_m2k.create_instr(m2k_ctx, "voltmeter")
 
 # Control signal generator
-volt0 = instr_m2k.control(ps, 0, [2, True])
-volt1 = instr_m2k.control(ps, 1, [-4, True])
-
-time.sleep(5)
+volt0 = instr_m2k.control(voltmeter, 0)
+print(f"Voltage at channel 0: {volt0} V")
+volt1 = instr_m2k.control(voltmeter, 1)
+print(f"Voltage at channel 1: {volt1} V")
 
 instr_m2k.contextClose(m2k_ctx)
+
+# ################### Power supply ###################
+# # Create power supply instrument from ip uri
+# m2k_uri = "ip:192.168.2.1"
+# m2k_ctx = instr_m2k.connect(uri=m2k_uri, calibrate=False)
+# ps = instr_m2k.create_instr(m2k_ctx, "powersupply")
+
+# # Control signal generator
+# volt0 = instr_m2k.control(ps, 0, [2, True])
+# volt1 = instr_m2k.control(ps, 1, [-4, True])
+
+# time.sleep(5)
+
+# instr_m2k.contextClose(m2k_ctx)
